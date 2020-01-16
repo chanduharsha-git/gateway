a6845ac5d30cea051149736173b9b0cd163330a4
BHARATH-JC <bharathjchari@gmail.com>
2020-01-16 13:44:00 +0530

    first commit

---
+++ src/main/java/com/bastech/gateway/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.bastech.gateway.config.audit;
---
+++ src/main/java/com/bastech/gateway/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.bastech.gateway.security;
+
+import com.bastech.gateway.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
